166722ec0054a633d27d508d73336b15576095ea
Chaitanya1005 <157682728+Chaitanya1005@users.noreply.github.com>
2024-01-26 12:15:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 8273336481', 'Hi! How are you? This is an automated message being sent with the help of python programming :).', 13, 54)
+pywhatkit.sendwhatmsg('+91 9368934899', 'Hi! How are you? This is an automated message being sent with the help of python programming :).', 13, 54)
